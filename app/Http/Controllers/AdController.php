2a112f0bdcbc7621f50bf08fd0e9b4b2b13f9f1d
alexsop <alexsandr246ilek@gmail.com>
2022-07-23 18:18:10 +0500

    Rename class

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -7,7 +7,7 @@ use App\Repositories\Interfaces\AdRepositoryInterface;
 use App\Http\Requests\StoreAdRequest;
 use App\Http\Requests\IndexAdRequest;
 use App\Http\Requests\ShowAdRequest;
-use App\Http\Resources\PaginatedAdCollection;
+use App\Http\Resources\AdPaginatedCollection;
 use App\Http\Resources\AdResource;
 
 class AdController extends Controller
@@ -22,7 +22,7 @@ class AdController extends Controller
     public function index(IndexAdRequest $request) {
         $items = $this->repository->getAll($request->all());
 
-        $formatted_items = new PaginatedAdCollection($items);
+        $formatted_items = new AdPaginatedCollection($items);
 
         return response()->json($formatted_items, 200);
     }
